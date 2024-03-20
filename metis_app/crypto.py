d902a8845dbf21a1b6a069fb989d37ffa91be0d3
Col Perks <wild.fauve@gmail.com>
2024-03-20 15:58:02 +1300

    add dynamic token claims to dataclass

--- metis_app/crypto.py
+++ metis_app/crypto.py
@@ -1,33 +1,56 @@
 import json
+from dataclasses import make_dataclass, dataclass
+from functools import reduce
 from typing import Tuple, Union, Optional
 from metis_crypto import jwk, jwt
 from pymonad.tools import curry
-import collections
 
 from metis_fn import monad, chronos
 from . import error
 
+
 class JwtDecodingError(error.BaseError):
     pass
 
+
+@dataclass
+class IdTokenClaims:
+    iss: str
+    sub: str
+    aud: str
+    iat: int
+    exp: int
+    azp: str
+
+
 class IdToken:
-    claims_value = collections.namedtuple('IdTokenClaims', ['iss', 'sub', 'aud', 'iat', 'exp', 'azp'])
 
     def __init__(self, jwt, claims=None):
         self.jwt = jwt
         self.claims = claims
 
+    def id_token_claim_types(self):
+        return IdTokenClaims.__annotations__.keys()
+
     def id_claims(self):
-        return self.claims_value(iss=self.claims['iss'],
-                                 sub=self.claims['sub'],
-                                 aud=self.claims['aud'],
-                                 iat=self.claims['iat'],
-                                 exp=self.claims['exp'],
-                                 azp=self.claims['azp'])
+        def only_id_token_claim(acc, claim_key):
+            clm = self.claims.get(claim_key, None)
+            if clm:
+                acc[claim_key] = clm
+            return acc
+
+        return IdTokenClaims(**reduce(only_id_token_claim, self.id_token_claim_types(), {}))
+
+    def all_claims(self):
+        cls = self._dynamic_dataclass()
+        return cls(**self.claims)
+
+    def _dynamic_dataclass(self):
+        return make_dataclass("TokenClaims", [(k, type(v).__name__) for k, v in self.claims.items()])
 
     def sub(self):
         return self.claims['sub']
-    
+
     def exp(self):
         return self.claims['exp']
 
@@ -41,14 +64,18 @@ def parse_generate_id_token(serialised_jwt, jwks=None, claims_to_assert=None):
     """
     return monad.Right(jwks) >> decode_jwt(claims_to_assert, serialised_jwt) >> to_id_token
 
+
 def bearer_token_valid(serialised_jwt: str) -> bool:
     return parse_generate_id_token(serialised_jwt) >> validate
+
+
 #
 # Helpers
 #
 
 def validate(token):
     return not token.expired()
+
 
 @curry(3)
 @monad.monadic_try(name="decode_jwt", status=401, error_cls=JwtDecodingError)
--- tests/test_crypto.py
+++ tests/test_crypto.py
@@ -16,6 +16,23 @@ def test_extract_claims_from_token():
 
     assert result.value.id_claims().sub == "1@clients"
 
+def test_a_claims_dataclass_dynamically():
+    jwt = fixture_crypto.generate_signed_jwt(fixture_crypto.Idp().jwk)
+
+    token = crypto.parse_generate_id_token(jwt, fixture_crypto.Idp().jwks_from_json()).value
+
+    all_claims = token.all_claims()
+    assert all_claims.exp
+    assert all_claims.iat
+    assert all_claims.sub
+    assert all_claims.azp
+    assert all_claims.gty
+    assert all_claims.iss
+    assert all_claims.aud
+
+
+
+
 def test_malformed_jwt():
     result = crypto.parse_generate_id_token(fixture_crypto.Idp().jwks_from_json(), "malformed")
 
